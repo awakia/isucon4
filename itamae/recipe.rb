8e6b5ce01cba0e51e53aafa34e44a4f4a1a21a0d
Daisuke Fujita <dtanshi45@gmail.com>
2015-09-10 14:42:05 +0900

    Change owner of configure files to dtan4

--- itamae/recipe.rb
+++ itamae/recipe.rb
@@ -2,3 +2,10 @@ package "redis" do
   action :install
   options "--enablerepo=epel"
 end
+
+%w(/etc/nginx/nginx.conf /etc/redis.conf /etc/my.cnf).each do |f|
+  file f do
+    action :edit
+    owner "dtan4"
+  end
+end
